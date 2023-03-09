272b31839737995b64b77d1e1c037da46a14c9fa
annie91annie91 <79952901+annie91annie91@users.noreply.github.com>
2023-03-09 16:43:10 +0800

    Add files via upload

---
+++ week04-2/main.cpp
@@ -0,0 +1,54 @@
+#include <GL/glut.h>
+const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
+
+const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
+
+void myLight()
+{
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING);
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+}
+
+float angle = 0;  //全域變數 angle角度
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);  //清畫面
+    glPushMatrix();
+        glRotatef(angle,0,1,0);  //轉angle度
+        glutSolidTeapot(0.3);
+    glPopMatrix();  //還原矩陣
+    glutSwapBuffers();  //交換顯示
+    angle++;
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);  //設定顯示模式
+    glutCreateWindow("week04");  //視窗名稱
+
+    myLight();
+    glutDisplayFunc(display);  //顯示的函式
+    glutIdleFunc(display);  //重畫畫面
+    glutMainLoop();  //主要迴圈最後
+}
---
+++ week04-4/main.cpp
@@ -0,0 +1,34 @@
+#include <GL/glut.h>
+float angle = 0;  //全域變數 angle角度
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);  //清畫面
+
+    glPushMatrix();
+        glTranslatef(0.5,0,0);  //出現在畫面右邊
+        glRotatef(angle,0,1,0);  //轉angle度
+        glColor3f(0,0,1);  //藍色
+        glutSolidTeapot(0.3);
+    glPopMatrix();  //還原矩陣
+
+    glPushMatrix();
+        glRotatef(angle,0,1,0);  //轉angle度
+        glTranslatef(0.5,0,0);  //出現在畫面右邊
+        glColor3f(1,1,0);  //黃色
+        glTranslatef(0,0.5,0);  //出現在畫面上方
+        glutSolidTeapot(0.3);
+    glPopMatrix();  //還原矩陣
+
+    glutSwapBuffers();  //交換顯示
+    angle++;
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);  //設定顯示模式
+    glutCreateWindow("week04");  //視窗名稱
+
+    glutDisplayFunc(display);  //顯示的函式
+    glutIdleFunc(display);  //重畫畫面
+    glutMainLoop();  //主要迴圈最後
+}
