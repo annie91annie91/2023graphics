9e0c664562e03a2651f043a1a3218b5f6b3876dc
annie91annie91 <79952901+annie91annie91@users.noreply.github.com>
2023-05-04 16:25:53 +0800

    Add files via upload

---
+++ week12/week12-2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char line[20];
+    //scanf("%s",line);
+    FILE*fin=fopen("file.txt","r"); //r:read讀
+    fscanf(fin,"%s",line);
+    printf("讀到了:%s\n",line); //馬上印
+    fscanf(fin,"%s",line);
+    printf("讀到了:%s\n",line); //馬上印
+}
---
+++ week12/week12-3.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int a[10]={10,20,30,40,50,60,70,80,90,100};
+    FILE*fout=fopen("file3.txt","w");
+    for(int i=0;i<10;i++)
+    {
+        fprintf(fout,"%d ",a[i]); //寫到檔案去
+        printf("%d ",a[i]); //順便印出來
+    }
+    fclose(fout);
+
+    int b[10];
+    FILE*fin=fopen("file3.txt","r");
+    for(int i=0;i<10;i++)
+    {
+        fscanf(fin,"%d",&b[i]);
+        printf("%d ",b[i]); //讀到陣列b[i]裡
+    }
+    fclose(fin);
+}
